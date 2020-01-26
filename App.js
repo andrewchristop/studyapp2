29e3e2a01357e75e7361438e6745d0f525e5f3ef
andrewchristop <andrewchristopher64@gmail.com>
2020-01-26 21:10:13 +0700

    Set-up firebase and root

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  createAppContainer,
+  createSwitchManager,
+  createSwitchNavigator
+} from "react-navigation";
+
+import * as firebase from "firebase";
+
+import { StyleSheet, Text, View } from "react-native";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCC0m9kYUq4u-dw7RSouKkBCmMZLwm3P68",
+  authDomain: "studyapp-587f7.firebaseapp.com",
+  databaseURL: "https://studyapp-587f7.firebaseio.com",
+  projectId: "studyapp-587f7",
+  storageBucket: "studyapp-587f7.appspot.com",
+  messagingSenderId: "510623847493",
+  appId: "1:510623847493:web:d692bfbefa4962e096ff2c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const RootStack = createSwitchNavigator(
+  {
+    Login: LoginScreen,
+    Signup: SignupScreen,
+    Main: MainNavigator
+  },
+
+  {
+    initialRouteName: "Login"
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <AppContainer></AppContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
