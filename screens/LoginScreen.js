ab106de28a06423d87b3fbe46c7e776add339020
andrewchristop <andrewchristopher64@gmail.com>
2020-04-22 10:32:29 +0700

    implemented login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,20 +5,39 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
-  View
+  View,
 } from "react-native";
 
 export default class LoginScreen extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    error: "",
+  };
+  handleLogin() {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => this.props.navigation.navigate("Main"))
+      .catch((error) => this.setState({ error: error.message }));
+    console.log("handleLogin");
+  }
   render() {
     return (
       <View style={style.body}>
-        <TextInput style={style.email} placeholder="Email"></TextInput>
+        <TextInput
+          style={style.email}
+          placeholder="Email"
+          onChangeText={(email) => this.setState({ email })}
+        ></TextInput>
         <TextInput
           style={style.password}
           placeholder="Password"
           secureTextEntry
+          onChangeText={(password) => this.setState({ password })}
         ></TextInput>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.handleLogin()}>
           <Text>Submit</Text>
         </TouchableOpacity>
       </View>
@@ -30,9 +49,9 @@ const style = StyleSheet.create({
   body: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   email: {
-    borderColor: "black"
-  }
+    borderColor: "black",
+  },
 });
